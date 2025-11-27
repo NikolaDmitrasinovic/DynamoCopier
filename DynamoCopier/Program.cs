e1308ade63f50d727f179a5cbe2db9a187025a29
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-11-27 10:46:46 +0100

    feat: resolve credentials

--- DynamoCopier/Program.cs
+++ DynamoCopier/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: dotnet run -- <SourceTable> <TargetTable>");
+            return;
+        }
+
+        var sourceTable = args[0];
+        var targetTable = args[1];
+
+        var regionName = Enviroment.GetEnviromentVariable("AWS_REGION") ?? "us-east-1";
+        var region = RegionEndpoint.GetBySystemName(regionName);
+
+        Console.WriteLine($"Region: {region.SystemName}");
+
+        using var client = new AmazonDynamoDBClient(region);
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error copying table:");
+            Console.ResetColor();
+            Console.WriteLine(ex);
+        }
+    }
+}
